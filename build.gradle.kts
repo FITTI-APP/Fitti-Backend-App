1f12995653999d1fbebc75740fb606712f6bfdb4
uuccu <sungu0804@gmail.com>
2023-05-01 01:34:24 +0900

    feat: 일단 실행 가능하게 하기

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,150 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	val kotlinVersion = "1.8.0"
+
 	id("org.springframework.boot") version "3.0.6"
 	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+
+	kotlin("jvm") version kotlinVersion
+	kotlin("plugin.spring") version kotlinVersion
+	kotlin("plugin.jpa") version kotlinVersion
+	kotlin("kapt") version kotlinVersion
 }
 
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
+version = "0.0.1"
 
 repositories {
 	mavenCentral()
 }
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
 	}
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+apply(plugin = "java")
+
+apply(plugin = "io.spring.dependency-management")
+apply(plugin = "org.springframework.boot")
+apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+
+apply(plugin = "kotlin")
+apply(plugin = "kotlin-spring") // all-open
+apply(plugin = "kotlin-jpa")
+apply(plugin = "kotlin-kapt")
+
+dependencies {
+	dependencyManagement {
+		imports {
+			mavenBom("de.codecentric:spring-boot-admin-dependencies:3.0.2")
+			mavenBom("io.micrometer:micrometer-bom:1.10.5")
+			mavenBom("io.micrometer:micrometer-tracing-bom:1.0.3")
+		}
+	}
+	val springDocVersion = "2.0.4"
+	val queryDslVersion = "5.0.0"
+//	val mapStructVersion = "1.5.3.Final"
+
+
+	fun amazon(module: String, version: String? = null) =
+		"software.amazon.awssdk:${module}${version?.let { ":$it" } ?: ""}"
+
+//	implementation(amazon("secretsmanager", "2.20.16"))
+//	implementation(amazon("ssm", "2.20.16"))
+//	implementation(amazon("elasticbeanstalk", "2.20.16"))
+//	implementation(amazon("s3", "2.20.16"))
+//	implementation("org.springframework.cloud", "spring-cloud-starter-aws", "2.2.6.RELEASE")
+//	implementation("org.springframework.boot", "spring-boot-configuration-processor", "2.6.3")
+//	implementation("com.auth0", "java-jwt", "4.3.0")
+
+
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	implementation("org.springframework.boot", "spring-boot-starter-actuator")
+	implementation("org.springframework.boot", "spring-boot-starter-batch")
+	implementation("org.springframework.boot", "spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot", "spring-boot-starter-data-jpa")
+//	implementation("org.springframework.boot", "spring-boot-starter-data-redis")
+	implementation("org.springframework.boot", "spring-boot-starter-graphql")
+	implementation("org.springframework.boot", "spring-boot-starter-security")
+	implementation("org.springframework.boot", "spring-boot-starter-validation")
+	implementation("org.springframework.boot", "spring-boot-starter-web")
+	implementation("org.springframework.boot", "spring-boot-configuration-processor")
+	implementation("com.h2database:h2")
+
+//	implementation("de.codecentric", "spring-boot-admin-starter-client")
+//	implementation("de.codecentric", "spring-boot-admin-starter-server")
+
+	implementation("org.springdoc", "springdoc-openapi-starter-common", springDocVersion)
+	implementation("org.springdoc", "springdoc-openapi-starter-webmvc-ui", springDocVersion)
+
+	implementation("io.micrometer", "micrometer-observation")
+	implementation("io.micrometer", "micrometer-tracing")
+	implementation("io.micrometer", "micrometer-tracing-bridge-brave")
+	implementation("io.micrometer", "context-propagation")
+
+	implementation("org.jetbrains.kotlin", "kotlin-reflect", "1.8.10")
+	implementation("org.jetbrains.kotlin", "kotlin-stdlib", "1.8.10")
+
+//	implementation("org.liquibase", "liquibase-core", "4.19.1")
+
+	implementation("com.querydsl", "querydsl-jpa", queryDslVersion, classifier = "jakarta")
+	implementation("com.querydsl", "querydsl-kotlin", queryDslVersion)
+	kapt("com.querydsl", "querydsl-apt", queryDslVersion, classifier = "jakarta")
+	implementation("jakarta.persistence", "jakarta.persistence-api")
+	implementation("jakarta.annotation", "jakarta.annotation-api")
+//	implementation("com.tailrocks.graphql", "graphql-datetime-spring-boot-starter", "6.0.0")
+//	implementation("com.graphql-java:graphql-java-extended-scalars:20.0")
+//	implementation("org.mapstruct", "mapstruct", mapStructVersion)
+//	kapt("org.mapstruct", "mapstruct-processor", mapStructVersion)
+
+//	implementation("com.google.guava", "guava", "31.1-jre")
+//	api("com.google.guava", "guava", "31.1-jre")
+
+	//mysql 관련 의존성 추가 예정
+	developmentOnly("org.springframework.boot", "spring-boot-devtools")
+	annotationProcessor("org.springframework.boot", "spring-boot-configuration-processor")
+
+
+//	kaptTest("org.mapstruct", "mapstruct-processor", mapStructVersion)
+	testImplementation("io.kotest", "kotest-runner-junit5", "5.5.5")
+	testImplementation("io.kotest", "kotest-assertions-core", "5.5.5")
+	testImplementation("io.kotest", "kotest-property", "5.5.5")
+	testImplementation("io.kotest.extensions", "kotest-extensions-spring", "1.1.2")
+	testImplementation("io.kotest.extensions", "kotest-property-arbs", "2.1.2")
+	testImplementation("io.mockk", "mockk", "1.13.4")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework", "spring-webflux", "6.0.6")
+	testImplementation("org.springframework.batch", "spring-batch-test")
+	testImplementation("org.springframework.graphql", "spring-graphql-test")
+	testImplementation("org.springframework.security", "spring-security-test")
+
+//	testRuntimeOnly("com.h2database", "h2")
+//	testRuntimeOnly("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.6.4")
+//	testRuntimeOnly("org.jetbrains.kotlinx", "kotlinx-coroutines-test", "1.6.4")
 }
+
+tasks{
+	withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "17"
+		}
+	}
+
+	withType<Test> {
+		useJUnitPlatform()
+	}
+}
+
+allOpen {
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
+	annotation("jakarta.persistence.Entity")
+}
+
