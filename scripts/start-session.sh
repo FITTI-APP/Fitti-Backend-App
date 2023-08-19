b2677bc76ae53da4a6fda4f88ff1b14060c3468e
uuccu <sungu0804@gmail.com>
2023-08-19 23:57:28 +0900

    Update start-session.sh

--- scripts/start-session.sh
+++ scripts/start-session.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-aws ssm start-session \
---target i-077dc234214b32607 \
---document-name AWS-StartPortForwardingSessionToRemoteHost \
---parameters '{"host":["fitty-develop-database.cmzpqp9pc6gs.ap-northeast-2.rds.amazonaws.com"],"portNumber":["3306"], "localPortNumber":["5510"]}'
+if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
+  export AWS_DEFAULT_REGION=ap-northeast-2
+  export AWS_PROFILE=fitty
+fi
 
 aws ssm start-session \
---target i-011aae92b218d1aa7 \
+--profile=fitty \
+--target i-072c1871abe75ae7e \
 --document-name AWS-StartPortForwardingSessionToRemoteHost \
---parameters '{"host":["awseb-e-sm8j3aindp-stack-awsebrdsdatabase-hp55tcxbhajy.cmzpqp9pc6gs.ap-northeast-2.rds.amazonaws.com"],"portNumber":["3306"], "localPortNumber":["5511"]}'
+--parameters '{"host":["fitty-develop-database.cmzpqp9pc6gs.ap-northeast-2.rds.amazonaws.com"],"portNumber":["3306"], "localPortNumber":["5510"]}'
