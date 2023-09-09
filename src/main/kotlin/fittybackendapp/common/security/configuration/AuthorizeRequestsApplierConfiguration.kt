1eb7230d750d2a9400f3e375052d50b785dcef73
uuccu <sungu0804@gmail.com>
2023-09-09 16:32:13 +0900

    feat: 설정이 완료되었습니다.

--- src/main/kotlin/fittybackendapp/common/security/configuration/AuthorizeRequestsApplierConfiguration.kt
+++ src/main/kotlin/fittybackendapp/common/security/configuration/AuthorizeRequestsApplierConfiguration.kt
@@ -10,9 +10,34 @@ class AuthorizeRequestsApplierConfiguration {
     @Bean
     fun authorizeRequestsApplier(): AuthorizeRequestsApplier {
         return { http ->
-            http.authorizeHttpRequests {
-                it.anyRequest().permitAll()
-            }
+            http.authorizeHttpRequests()
+                .requestMatchers(
+                    *URLS_ADMIN,
+                    *URLS_GRAPHQL,
+                    *URLS_DOCUMENT,
+                )
+                .permitAll()
+                .anyRequest() // todo 나중을 기약
+                .authenticated()
         }
+    }
+
+    companion object {
+        private val URLS_AUTHENTICATION = arrayOf(
+            "auth",
+            "auth/register",
+            "auth/login",
+        )
+        private val URLS_GRAPHQL = arrayOf(
+            "graphql",
+        )
+        private val URLS_ADMIN = arrayOf(
+            "admin",
+        )
+        private val URLS_DOCUMENT = arrayOf(
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/graphiql",
+        )
     }
 }
