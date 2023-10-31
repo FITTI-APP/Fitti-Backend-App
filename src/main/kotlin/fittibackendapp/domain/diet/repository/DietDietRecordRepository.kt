749c649c5f8ee46f4a617bae6072d43f05da66c4
Jaeheon Kim <xhdtlsid2@gmail.com>
2023-11-01 01:28:56 +0900

    feat: repository 생성

---
+++ src/main/kotlin/fittibackendapp/domain/diet/repository/DietDietRecordRepository.kt
@@ -0,0 +1,8 @@
+package fittibackendapp.domain.diet.repository
+
+import fittibackendapp.domain.diet.entity.DietDietRecord
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface DietDietRecordRepository : JpaRepository<DietDietRecord, Long>{
+    fun findAllByDietRecordId(dietRecordId: Long): List<DietDietRecord>
+}
