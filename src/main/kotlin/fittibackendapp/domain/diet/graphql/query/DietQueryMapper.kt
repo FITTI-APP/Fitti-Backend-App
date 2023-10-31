3363c4874e0060efbcb9e83bcb6cee48b16c8003
Jaeheon Kim <xhdtlsid2@gmail.com>
2023-11-01 01:29:58 +0900

    feat: DietQueryMapper

---
+++ src/main/kotlin/fittibackendapp/domain/diet/graphql/query/DietQueryMapper.kt
@@ -0,0 +1,25 @@
+package fittibackendapp.domain.diet.graphql.query
+
+import fittibackendapp.domain.diet.facade.DietRecordQueryFacade
+import fittibackendapp.dto.PcfAmountInGramsDto
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+
+@RestController
+class DietQueryMapper(
+    private val dietRecordQueryFacade: DietRecordQueryFacade
+){
+    @QueryMapping
+    fun getPcfAmountInGramsBetweenDays(
+        @Argument
+        fromDate: LocalDate,
+        @Argument
+        toDate: LocalDate
+    ): PcfAmountInGramsDto {
+        val userId = 1L
+
+        return dietRecordQueryFacade.getPcfAmountInGramsBetweenDays(userId, fromDate, toDate)
+    }
+}
