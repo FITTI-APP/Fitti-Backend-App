d9c945f0d0f21997c639fd04dd6ca68c8e0d1c03
Jaeheon Kim <xhdtlsid2@gmail.com>
2023-11-02 21:52:34 +0900

    feat: 쿼리 날리는 횟수 줄이기

--- src/main/kotlin/fittibackendapp/domain/diet/facade/DietRecordQueryFacade.kt
+++ src/main/kotlin/fittibackendapp/domain/diet/facade/DietRecordQueryFacade.kt
@@ -14,18 +14,18 @@ class DietRecordQueryFacade (
     private val dietDietRecordService: DietDietRecordService
 ){
     fun getPcfAmountInGramsBetweenDays(userId: Long, fromDate: LocalDate, toDate: LocalDate) : PcfAmountInGramsDto {
-        val dietRecords = dietRecordService.findDietRecordsBetweenDays(userId, fromDate, toDate)
+        val dietRecordIds = dietRecordService.findDietRecordsBetweenDays(userId, fromDate, toDate).map { it.id }
+        val dietDietRecords = dietDietRecordService.findAllByDietRecordIdIn(dietRecordIds)
+        val diets = dietService.findAllById(dietDietRecords.map { it.diet.id }.distinct())
+        val dietMap = diets.associateBy { it.id }
         var protein = 0.0
         var carbohydrate = 0.0
         var fat = 0.0
-        dietRecords.forEach { dietRecord ->
-            val dietDietRecords = dietDietRecordService.findAllByDietRecordId(dietRecord.id)
-            dietDietRecords.forEach {
-                val diet = dietService.findById(it.diet.id)
-                protein += diet.protein * it.weight
-                carbohydrate += diet.carbohydrate * it.weight
-                fat += diet.fat * it.weight
-            }
+        dietDietRecords.forEach {
+            val diet = dietMap[it.diet.id]!!
+            protein += diet.protein * it.weight
+            carbohydrate += diet.carbohydrate * it.weight
+            fat += diet.fat * it.weight
         }
         return PcfAmountInGramsDto(protein, carbohydrate, fat)
     }
