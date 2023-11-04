a90198784d9fa04229d2a34f8a5f01821f8e6c9b
정재현 <59111693+dicohy27@users.noreply.github.com>
2023-11-04 20:47:06 +0900

    unused import 삭제

--- src/main/kotlin/fittibackendapp/domain/exercise/repository/ExerciseRepository.kt
+++ src/main/kotlin/fittibackendapp/domain/exercise/repository/ExerciseRepository.kt
@@ -1,7 +1,6 @@
 package fittibackendapp.domain.exercise.repository
 
 import fittibackendapp.domain.exercise.entity.Exercise
-import fittibackendapp.domain.exercise.entity.ExerciseRecord2
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ExerciseRepository: JpaRepository<Exercise, Long> {
