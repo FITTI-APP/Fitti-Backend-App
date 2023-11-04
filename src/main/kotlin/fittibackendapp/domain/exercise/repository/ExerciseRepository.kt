afcfab3ff5f5a53b8d9ea8181f3eca4662631a56
정재현 <59111693+dicohy27@users.noreply.github.com>
2023-11-04 19:17:34 +0900

    운동 타입으로 운동 목록 조회

--- src/main/kotlin/fittibackendapp/domain/exercise/repository/ExerciseRepository.kt
+++ src/main/kotlin/fittibackendapp/domain/exercise/repository/ExerciseRepository.kt
@@ -1,6 +1,9 @@
 package fittibackendapp.domain.exercise.repository
 
 import fittibackendapp.domain.exercise.entity.Exercise
+import fittibackendapp.domain.exercise.entity.ExerciseRecord2
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ExerciseRepository: JpaRepository<Exercise, Long>
+interface ExerciseRepository: JpaRepository<Exercise, Long> {
+    fun findAllByKindId(kindId: Long?): List<Exercise>
+}
