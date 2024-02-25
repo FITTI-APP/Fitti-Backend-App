5e071e35d0c92e6e034e5a81431155b59cf32446
Jaeheon Kim <xhdtlsid2@gmail.com>
2024-02-25 18:11:59 +0900

    userId 대신 user를 들고 있도록

--- src/main/kotlin/fittibackendapp/domain/diet/entity/DietMealRecord.kt
+++ src/main/kotlin/fittibackendapp/domain/diet/entity/DietMealRecord.kt
@@ -1,22 +1,30 @@
 package fittibackendapp.domain.diet.entity
 
 import fittibackendapp.common.entitiybase.AuditLoggingBase
+import fittibackendapp.domain.auth.entity.User
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
 import java.time.LocalDateTime
 
 @Entity
 @Table(name = "diet_meal_record")
 class DietMealRecord(
     @NotNull
-    @Column(name = "user_id", nullable = false)
-    var userId: Long,
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id", nullable = false)
+    var user: User,
     @NotNull
     @Column(name = "date_time", nullable = false)
     var dateTime: LocalDateTime,
