082fe8c7a14b25cbe663d6ba940efdd98a9c8936
uuccu <sungu0804@gmail.com>
2024-03-10 22:07:06 +0900

    Update DietMealRecordRepository.kt

--- src/main/kotlin/fittibackendapp/domain/diet/repository/DietMealRecordRepository.kt
+++ src/main/kotlin/fittibackendapp/domain/diet/repository/DietMealRecordRepository.kt
@@ -4,10 +4,12 @@ import fittibackendapp.domain.diet.entity.DietMealRecord
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
 import java.time.LocalDate
 
+@Repository
 interface DietMealRecordRepository: JpaRepository<DietMealRecord, Long> {
-    @Query("SELECT e FROM DietMealRecord e WHERE e.userId = :userId AND e.dateTime BETWEEN :fromDate AND :toDate")
+    @Query("SELECT e FROM DietMealRecord e WHERE e.user.id = :userId AND e.dateTime BETWEEN :fromDate AND :toDate")
     fun findDietMealRecordsBetweenDays(
         @Param("userId")
         userId: Long,
